5c2df8882dea9cd9d9a48004aa97b823af836e1d
FatimaIqbal <12bscsfiqbal@seecs.edu.pk>
2015-05-28 00:02:47 +0500

    Create vertex_with_order.java

---
+++ vertex_with_order.java
@@ -0,0 +1,14 @@
+
+package model.pojo;
+
+
+public class Vertex_with_order {
+    public String vertex;
+    public int order;
+    
+    Vertex_with_order(){}
+    public Vertex_with_order(String a,int b){
+        this.vertex=a;
+        this.order=b;
+    }
+}
